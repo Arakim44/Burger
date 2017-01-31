d635d088d47aa6a3388a3207d1051113fa36a963
Ara Kim <ahrakim44@gmail.com>
2017-01-31 11:48:15 -0600

    controller and model written

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,4 +5,38 @@ var router = express.Router();
 // Import the model (burger.js) to use it's database functions.
 var burger = require("../models/burger.js");
 
+router.get("/",function(req, res){
+  burger.all(function(data){
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/", function(req, res){
+  burger.create([
+    "burger_name","devoured"
+  ],[
+    req.body.burger_name, req.body.devoured
+  ], function(){
+    res.redirect("/");
+  });
+});
+
+
+router.put("/:id", function(req,res){
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function(){
+    res.redirect("/");
+  });
+});
+
+//Export foutes for server.js to use
 module.exports = router;
